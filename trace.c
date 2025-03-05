fujitsu/crash-trace

commit c5913a960dfa07d64397e6b591e6f0d40ea0b503

trace: Support module memory layout change on Linux 6.4
Linux kernel introduced new module memory layout on Linux 6.4 at the
commit ac3b432 (module: replace module_layout with module_memory) and
crash utility supported it at the commit 7750e61 (Support module
memory layout change on Linux 6.4).

In the change, crash utility changed definition and usage of data
structure for kernel modules. However, crash trace also refers to the
data structures. As a result, with the crash utility with the change,
crash trace command results in segmentation fault:

  crash> extend trace.so
  /usr/lib64/crash/extensions/trace.so: shared object loaded
  crash> trace show
  Segmentation fault (core dumped)

To fix this issue, let's support the new definition and usage of
struct load_module in crash trace. This is at the same time intended
for crash trace to support the new module memory layout of the Linux
kernel.

On the other hand, there is still inevitable dependency left to size
of struct load_module, one of the data structures of crash utility, in
crash trace. struct symbol_table_data holds an array of objects of
struct load_module in member variable load_modules and we need to know
size of struct load_module in advance to refer to its elements.

  struct symbol_table_data {
  ...snip...
        int mods_installed;
        struct load_module *current;
        struct load_module *load_modules;

Due to this, once crash trace is built with the defs.h including
change of size of struct load_module, it can never be used with the
older crash utility that was built with a different size of struct
load_module.

Because there is no preferable way to tackle the dependency without
altering implementation of crash utility, we have no choice but accept
breaking backward-compatibility for the time being.

Signed-off-by: HATAYAMA Daisuke <d.hatayama@fujitsu.com>

trace.c
@@ -2219,13 +2219,10 @@ fail:
 	return -1;
 }
 
-static int save_proc_kallsyms(int fd)
+static void __save_proc_kallsyms_mod_legacy(void)
 {
 	int i;
 	struct syment *sp;
-
-	for (sp = st->symtable; sp < st->symend; sp++)
-		tmp_fprintf("%lx %c %s\n", sp->value, sp->type, sp->name);
 
 	for (i = 0; i < st->mods_installed; i++) {
 		struct load_module *lm = &st->load_modules[i];
@@ -2239,6 +2236,50 @@ static int save_proc_kallsyms(int fd)
 					sp->name, lm->mod_name);
 		}
 	}
+}
+
+#ifdef MODULE_MEMORY
+static void __save_proc_kallsyms_mod_v6_4(void)
+{
+	int i, t;
+	struct syment *sp;
+
+        for (i = 0; i < st->mods_installed; i++) {
+                struct load_module *lm = &st->load_modules[i];
+
+                for_each_mod_mem_type(t) {
+                        if (!lm->symtable[t])
+				continue;
+
+                        for (sp = lm->symtable[t]; sp <= lm->symend[t]; sp++) {
+                                if (!strncmp(sp->name, "_MODULE_", strlen("_MODULE_")))
+                                        continue;
+
+                                /* Currently sp->type for modules is not trusted */
+                                tmp_fprintf("%lx %c %s\t[%s]\n", sp->value, 'm',
+                                            sp->name, lm->mod_name);
+                        }
+                }
+	}
+}
+#else
+#define MODULE_MEMORY() (0)
+static inline void __save_proc_kallsyms_mod_v6_4(void)
+{
+}
+#endif
+
+static int save_proc_kallsyms(int fd)
+{
+	struct syment *sp;
+
+	for (sp = st->symtable; sp < st->symend; sp++)
+		tmp_fprintf("%lx %c %s\n", sp->value, sp->type, sp->name);
+
+	if (MODULE_MEMORY())
+		__save_proc_kallsyms_mod_v6_4();
+	else
+		__save_proc_kallsyms_mod_legacy();
 
 	if (tmp_file_record_size4(fd))
 		return -1;

commit 2106a9d04020192f6f286171a060c4d2300059b7

Fix segmentation fault during executing trace dump -s
Currently, trace dump -s results in segmentation fault on the vmcore
corresponding to kernel-core-6.13.5-200.fc41.x86_64:

  crash> trace dump -s trace_dump_dir
  Segmentation fault (core dumped)

This is caused by the commit c5913a960dfa07d64397e6b591e6f0d40ea0b503
(trace: Support module memory layout change on Linux 6.4) that added
support of the module memory layout change on Linux kernel 6.4, where
the implementation on function dump_kallsyms() was overlooked.

Fix this issue by introducing a new version of dump_kallsyms() for the
new module memory layout.

trace.c
@@ -1628,7 +1628,52 @@ static int dump_saved_cmdlines(const char *dump_tracing_dir)
 	return 0;
 }
 
-static int dump_kallsyms(const char *dump_tracing_dir)
+#ifdef MODULE_MEMORY
+static int dump_kallsyms_mod_v6_4(const char *dump_tracing_dir)
+{
+	char path[PATH_MAX];
+	FILE *out;
+	int i, t;
+	struct syment *sp;
+
+	snprintf(path, sizeof(path), "%s/kallsyms", dump_tracing_dir);
+	out = fopen(path, "w");
+	if (out == NULL)
+		return -1;
+
+	for (sp = st->symtable; sp < st->symend; sp++)
+		fprintf(out, "%lx %c %s\n", sp->value, sp->type, sp->name);
+
+	for (i = 0; i < st->mods_installed; i++) {
+		struct load_module *lm = &st->load_modules[i];
+
+		for_each_mod_mem_type(t) {
+			if (!lm->symtable[t])
+				continue;
+
+			for (sp = lm->symtable[t]; sp <= lm->symend[t]; sp++) {
+				if (!strncmp(sp->name, "_MODULE_", strlen("_MODULE_")))
+					continue;
+
+				/* Currently sp->type for modules is not trusted */
+				fprintf(out, "%lx %c %s\t[%s]\n", sp->value, 'm',
+					sp->name, lm->mod_name);
+			}
+		}
+	}
+
+	fclose(out);
+	return 0;
+}
+#else
+#define MODULE_MEMORY() (0)
+static int dump_kallsyms_mod_v6_4(const char *dump_tracing_dir)
+{
+	return 0;
+}
+#endif
+
+static int dump_kallsyms_legacy(const char *dump_tracing_dir)
 {
 	char path[PATH_MAX];
 	FILE *out;
@@ -1700,7 +1745,10 @@ static int populate_ftrace_dir_tree(struct trace_instance *ti,
 
 	if (flags & FTRACE_DUMP_SYMBOLS) {
 		/* Dump all symbols of the kernel */
-		dump_kallsyms(root);
+		if (MODULE_MEMORY())
+			dump_kallsyms_mod_v6_4(root);
+		else
+			dump_kallsyms_legacy(root);
 	}
 
 	return TRUE;
@@ -2263,7 +2311,6 @@ static void __save_proc_kallsyms_mod_v6_4(void)
 	}
 }
 #else
-#define MODULE_MEMORY() (0)
 static inline void __save_proc_kallsyms_mod_v6_4(void)
 {
 }